6026180a990dfd085f886850add294e2dee0fd5d
gyoenge <gyoeng94@gmail.com>
2026-04-20 22:37:20 +0900

    fix: modify patch mask saving logics

--- src/h5radiomics/engines/extract.py
+++ src/h5radiomics/engines/extract.py
@@ -884,6 +884,38 @@ def process_cellseg_patch(
             mask_filename=f"{patch.base_filename}__cellseg_all",
         )
 
+    # if save_patches:
+    #     for class_name, sub in patch_cellseg.groupby("class_name"):
+    #         if len(sub) == 0:
+    #             continue
+
+    #         mask_cls = rasterize_geometries_to_mask(
+    #             sub.geometry.tolist(),
+    #             image_shape=patch.gray_patch.shape,
+    #             label=label,
+    #         )
+
+    #         save_region_mask_images(
+    #             color_patch=patch.color_patch,
+    #             gray_patch=patch.gray_patch,
+    #             mask_patch=mask_cls,
+    #             output_dir=output_dir,
+    #             sample_id=sample_id,
+    #             mask_filename=f"{patch.base_filename}__cellseg_{class_name}",
+    #         )
+
+    if save_patches:
+        threshold_mask = build_threshold_mask(patch.gray_patch, label=label)
+
+        save_region_mask_images(
+            color_patch=patch.color_patch,
+            gray_patch=patch.gray_patch,
+            mask_patch=threshold_mask,
+            output_dir=output_dir,
+            sample_id=sample_id,
+            mask_filename=f"{patch.base_filename}__threshold",
+        )
+
     safe_update_features(
         row,
         lambda: extract_patch_level_radiomics(patch.gray_patch, extractor, label=label),
